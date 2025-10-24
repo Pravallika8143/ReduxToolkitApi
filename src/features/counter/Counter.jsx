c5da6b47d92bedb3fa9dd358d4eba50616bf9037
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-10-24 11:52:21 +0530

    counter

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -8,7 +8,7 @@ function Counter() {
      var dispatch = useDispatch();
     return (
         <div className="border border-3 m-3 p-3 border-dark">
-            <h1>Counter::{count}</h1>
+            <h1>Counter:{count}</h1>
             <button  style={{marginRight:"2%"}}onClick={()=>{dispatch(inc())}}>Increment</button>
             <button onClick={()=>{dispatch(dec())}}>Decrement</button>
         </div>
